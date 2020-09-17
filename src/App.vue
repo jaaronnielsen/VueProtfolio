8d47724eb4b17d3fbf5e6f39b8d37bda5e3e9038
Jaaron Nielsen <nielsenjaaron@gmail.com>
2020-09-16 22:41:28 -0600

    added skills

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,478 @@
 <template>
   <div id="app">
    <h1>Jaaron Nielsen's Portfolio</h1>
-    <Resume msg="Welcome to My Portfolio"/>
+    <img alt="Resume" src="./assets/resume.png">
+    <h3>Have Skills</h3>
+<br />
+<h4>Languages</h4>
+<div class="row">
+    <div class="col">
+        PHP:
+    </div>
+    <div class="col">
+        BambooHr develops their backend in PHP and I am a bachend intern at BambooHr so I work with PHP five days a week.
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+        Java:
+    </div>
+    <div class="col">
+        Worked as a team to create a multi threaded gas station simulation. That simulated cars randomly going to a pump and filling up with gas. 
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+        JavaScript:
+    </div>
+    <div class="col">
+        Created a game that allowed the player to navigate through a board doing the events that appeared on each tile. The logic was dictated by JavaScript.
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+        C#:
+    </div>
+    <div class="col">
+        This website is made in C# and Blazor. 
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+        SQL:
+    </div>
+    <div class="col">
+        I've used stored procedures and fucntions to create an ebay style bidding system. I've also created tables, joins, indexs, and CRUD operations in MySql at work. 
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+        Elixir:
+    </div>
+    <div class="col">
+        Created Conway's game of life and a black jack game using Elixir
+    </div>
+</div>
+<br />
+<h4>Soft Skills</h4>
+<div class="row">
+    <div class="col">
+        Object Oriented Programming:
+    </div>
+    <div class="col">
+        I use object oriented principles at work to create new classes that inherit from a common base class. 
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+        Jira:
+    </div>
+    <div class="col">
+        I've created stories using Jira. I use Jira at work to keep track of stories and epics. 
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+       CI/CD:
+    </div>
+    <div class="col">
+        At BambooHr we use CI/CD to push code to production. I've marked the PR with the proper lables to have it get picked up by CIhub and onto a train then pushed to production. 
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+        Agile/Scrum:
+    </div>
+    <div class="col">
+        I work in an Scrum eniroment at work. I report each day in stand up what I did the day before, what I'm doing today, and what roadblocks I have. 
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+        UML:
+    </div>
+    <div class="col">
+        I've created UML class diagrams to model how classes interact with eachother
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+        Communication:
+    </div>
+    <div class="col">
+        When I was a dev/project lead at work I had to communcate to the different members on the team and with product to get an questions answered and work delgated. 
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+       Git:
+    </div>
+    <div class="col">
+        I use GIt as version control at work and at school. I can do simple things like pushing, pulling, merging, cloneing, and branching.
+    </div>
+</div>
+<br />
+<div class="row">
+    <div class="col">
+        Unit Testing:
+    </div>
+    <div class="col">
+        At school we are encouraged to practice TDD and at work I have writen tests in PHPunit to test various parts of the code that I have written or updated. 
+    </div>
+</div>
+<br/>
+  <h4>Don't Have Skills Prioritized</h4>
+    <br />
+    <div class="row">
+        <div class="col">
+            JavaScript Frameworks (React, Vue, Node):
+        </div>
+        <div class="col">
+            Will be learning React in GUI, Plan on Making a version of my portfolio in Vue.
+        </div>
+        <div class="col">
+            15 hours to learn both frameworks
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Responsive Design:
+        </div>
+        <div class="col">
+            I will use responive design elements when I make the Vue and React versions of my Portfolio
+        </div>
+        <div class="col">
+            Part of the 15 hours for React and Vue.
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Front End Development Experience:
+        </div>
+        <div class="col">
+            Building a Vue and React UI will be the experience.
+        </div>
+        <div class="col">
+            15 hours will be apart of the Frameworks hours.
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            GoLang:
+        </div>
+        <div class="col">
+            Take the API made in C# and remake it in python
+        </div>
+        <div class="col">
+            Take me 7 hours to Learn.
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            TypeScript:
+        </div>
+        <div class="col">
+            I will use TypeScript in my react portfolio UI. 
+        </div>
+        <div class="col">
+            Take me 3 hours to Learn.
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            JSON:
+        </div>
+        <div class="col">
+            In GUI we are building a API to send data to the UI layers that we are making for the porfolio. I will use JSON to send data from the API to the UI.
+        </div>
+        <div class="col">
+            Take me 3 hours to Learn
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Python:
+        </div>
+        <div class="col">
+            Take the API made in C# and remake it in python
+        </div>
+        <div class="col">
+            Take me 7 hours to Learn
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            REST API:
+        </div>
+        <div class="col">
+            In GUI we are building a API to send data to the UI layers that API will be a REST API.
+        </div>
+        <div class="col">
+            Take me 3 hours to Learn
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Docker:
+        </div>
+        <div class="col">
+            I learn how to host my database for the API in Docker.
+        </div>
+        <div class="col">
+            Take me 3 hours to Learn
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            UI/UX Development Experience:
+        </div>
+        <div class="col">
+            While building the Final Project in GUI I will use UI/UX tools to design the project.
+        </div>
+        <div class="col">
+            Take me 8 hours to Learn
+        </div>
+    </div>
+    <br/>
+
+    <h4>Don't Have Skills Un-Prioritized</h4>
+    <br />
+    <div class="row">
+        <div class="col">
+            Multiple different programming paradigm
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Unit Testing with Selenium, Cucumber.
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            2+ years of full-time work experience as a software engineer
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Open source
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Full stack developer a plus
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Google Cloud Platform
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Mobile Frameworks
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            5 years od experience with front end and back end
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            AWS/LAMP environment
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            GraphQL
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            MEAN stack and/or Rails experience
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Photoshop
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            WordPress
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Blockchain Experience (Dash, Bitcoin)
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Familiar with Apache NiFi to create and manage data extract, transform, load (ETL) operations for critical data flows
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Redis
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            kafka
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Envoy
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Security
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            KnockoutJS
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Entity Framework
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Experience with Three.js and 3D modeling
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Ruby
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            OWASP Top 10
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            SOLID/DDD/CQRS patterns knowledge
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            CQRS/ES/Actor knowledge
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Experience With Scaleable Web Services
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            MVC
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Create Architectural System Design and Analysis
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Cross-Browser, Cross-Platform testing experience
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Shopify
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Experience with Elastic Stack technologies (ElasticSearch, Logstash, Kibana) to provide metrics and monitoring of distributed microservices
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Automated Deployment Strategies
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Experience working with authentication systems and protocols such as OAuth, Open ID, SAML, JWT, etc a plus
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            gRPC
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col">
+            Websockets
+        </div>
+    </div>
+    <br />
   </div>
 </template>
 
@@ -22,6 +493,7 @@ h1 {
   border-bottom: solid;
   border-color: darkslategrey;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
