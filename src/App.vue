3465811329bec7efd85e69a780ca699ecbc6b391
Jaaron Nielsen <nielsenjaaron@gmail.com>
2020-09-16 21:56:45 -0600

    vue app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Jaaron Nielsen's Portfolio</h1>
+    <Resume msg="Welcome to My Portfolio"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+// export default {
+//   name: 'App',
+//   components: {
+//     Resume
+//   }
+// }
 </script>
 
 <style>
+h1 {
+  text-align: center;
+  border-bottom: 5pt;
+  border-bottom: solid;
+  border-color: darkslategrey;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
 }
 </style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="skills">
+    <h1>{{ msg }}</h1>
+    <h3>Languages</h3>
+    <ul>
+      <li>PHP</li>
+      <li>Java</li>
+    </ul>
+    <h3>Soft skills</h3>
+    <ul>
+      <li>Core Docs</li>
+      <li>Forum</li>
+      <li>Community Chat</li>
+      <li>Twitter</li>
+      <li>News</li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li>vue-router</li>
+      <li>vuex</li>
+      <li>vue-devtools</li>
+      <li>vue-loader</li>
+      <li>awesome-vue</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Resume',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
